ec164625c646a25c9abe2c39310be5a727fc6599
Lucas Ribeiro da Costa <lucas.costa@benner.com.br>
2019-11-12 14:57:07 -0300

    TextBox for view token

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let result = document.getElementById('result')
 
-function teste() {
-    const uri = 'https://localhost:5001/Auth/login'
+function login() {
+    const loginUri = 'https://localhost:5001/Auth/login'
 
-    fetch(uri, { 
+    fetch(loginUri, { 
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ username: 'lucas', password: '123'})
+        body: JSON.stringify({ username: 'lucas', password: '123' })
      })
         .then(response => response.text())
         .then(text => result.innerText = text)
